11d402a5d50ae654e6719be1ea7a297c1dd2ab39
Ishwarya-H <ishwarya.h@cognizant.com>
2026-08-20 11:42:55 +0530

    feat: basic react layout with reusable components

---
+++ frontend/src/components/Button.jsx
@@ -0,0 +1,10 @@
+import './Button.scss';
+function Button({ label, onClick, variant = "primary" }) {
+  return (
+    <button className={`btn btn-${variant}`} onClick={onClick}>
+      {label}
+    </button>
+  );
+}
+
+export default Button;
